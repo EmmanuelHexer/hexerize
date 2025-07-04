ce8970e44f27e3a2d31ade095802fde3e40e27e1
izen webDev <kwabenaizen0531@gmail.com>
2025-07-04 12:04:30 +0000

    still ongoing tho

--- frontend/src/Pages/HowItWorks.tsx
+++ frontend/src/Pages/HowItWorks.tsx
@@ -7,7 +7,7 @@ const HowItWorks = () => {
         <p className="md:text-5xl mb-4 md:mb-0 text-3xl capitalize font-extrabold  leading-tight tracking-tight text-[color:var(--accent-color)] animate-slide-up">
           Map your Success
         </p>
-        <button className="px-6  text-white bg-[color:var(--accent-color)] hover:brightness-110 rounded-full shadow-xl ring-2 ring-[color:var(--accent-color)]/30 hover:ring-[color:var(--accent-color)]/50 transition-all transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring-[color:var(--accent-color)]/40 animate-slide-up overflow-hidden  py-4 duration-400 cursor-pointer hover:bg-green-100  font-semibold">
+        <button className="animate-slide-up px-6  text-white hover:brightness-110 bg-[color:var(--accent-color)]  rounded-full shadow-xl ring-2 ring-[color:var(--accent-color)]/30 hover:ring-[color:var(--accent-color)]/50 transition-all transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring-[color:var(--accent-color)]/40 animate-slide-up overflow-hidden  py-4 duration-400 cursor-pointer  font-semibold">
           Discover More
         </button>
       </div>
@@ -15,7 +15,7 @@ const HowItWorks = () => {
       <div className="flex flex-col md:flex-row mt-25 gap-4 px-10">
         <div className="text-center md:text-start">
           <p className="text-8xl font-semibold text-gray-400 mb-10">01</p>
-          <p className=" text-lg mb-5 capitalize text-[color:var(--body-color)]/70 font-semibold">
+          <p className=" text-lg mb-5 capitalize text-[color:var(--body-color)]/70 font-semibold ">
             get started
           </p>
           <p className="text-[color:var(--body-color)]/70 text-base md:text-lg ">
