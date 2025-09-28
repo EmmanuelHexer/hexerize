1b9b52ddfef5fbecb1e7922efe30cdf73ad185de
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-09-28 19:07:07 +0000

    new changes

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -4,16 +4,14 @@ import rightArrow from "./rightArrow.png";
 import whiteArrow from "./icons8-right-24.png";
 import openMenu from "./openMenu.png";
 import closeMenu from "./closeMenu.png";
-import Moon from "./Moon.svg";
-import Sun from "./Sun.svg";
 import darkLogo from "./hexerizeLogoDark.png";
 import project1 from "./projects/project1.jpeg";
 import project2 from "./projects/project2.jpeg";
 import dev1 from "./devs/dev1.jpg";
 import dev2 from "./devs/dev2.jpg";
 
-export { default as Sun } from "./sun.svg";
-export { default as Moon } from "./moon.svg";
+export { default as Sun } from "./Sun.svg";
+export { default as Moon } from "./Moon.svg";
 
 export const assets = {
   logo,
